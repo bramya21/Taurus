e3874e022e722fa202fe8586ef7a843fb860ab7b
sai krishna <raminisai33@gmail.com>
2019-08-29 15:02:05 +0530

    service file

--- accounts.service.ts
+++ accounts.service.ts
@@ -1,1 +1,55 @@
-/*********under integration with customer api's*****/
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Account } from '../models/Accounts'
+import { Transaction } from '../models/Transactions';
+import {User} from '../models/User'
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountsService {
+  id:string
+  accountsUrl:string = 'http://localhost:8075/rest/accounts'
+  transactionUrl:string = 'http://localhost:8075/rest/transaction'
+  loginUrl:string = 'http://localhost:8075/rest/login'
+
+  constructor(private http:HttpClient) {}
+
+  // Get accounts
+  getaccounts():Observable<Account[]> {
+    return this.http.get<Account[]>(this.accountsUrl);
+  }
+
+  // Delete account
+  deleteaccount(account:Account):Observable<Account> {
+    const url = `${this.accountsUrl}/${account.accno}`;
+    return this.http.delete<Account>(url, httpOptions);
+  }
+
+  // Add account
+  addaccount(account:Account):Observable<Account> {
+    return this.http.post<Account>(this.accountsUrl, account, httpOptions);
+  }
+  //get transactions by user account
+  getTransactions(transaction:Transaction):Observable<Transaction[]> {
+    const url = `${this.transactionUrl}/${transaction.fromaccount}`;
+    return this.http.get<Transaction[]>(url);
+  }
+  //add transaction
+  addTransaction(transaction:Transaction):Observable<Transaction>{
+    console.log(transaction)
+    console.log(this.http.put<Transaction>(this.transactionUrl,transaction,httpOptions))
+    return this.http.put<Transaction>(this.transactionUrl,transaction,httpOptions)
+  }
+  isValidLogin(user:User):Observable<User>{
+    return this.http.post<User>(this.loginUrl,user);
+  }
+
+}
